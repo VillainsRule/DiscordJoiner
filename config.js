e46c2c807d83fe8a51a1525560744e3e3fbf889f
thonk <101288516+VillainsRule@users.noreply.github.com>
2023-08-07 09:39:57 -0400

    add useProxies to config

--- config.js
+++ config.js
@@ -1,8 +1,13 @@
 module.exports = {
     captcha_service: '', // specify one. can be "2captcha" or "capmonster"
     captcha_api_key: '', // the API key for captcha access
+    
     invite: '', // the server invite code, example is 'HGfFFUQ7F7', can be vanity
+
+    useProxies: false, // if to use proxies from proxies.txt to join
+    
     joinDelay: 1000, // how fast to join in milliseconds (too fast = ratelimit!)
+    
     boost: {
         enabled: false, // if to boost any server
         delay: 2000, // the delay to boost in millseconds (too fast = ratelimit!)
